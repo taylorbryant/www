1d62e1e9d6796b2adf7fcb15d681765cda86a98b
Taylor Bryant <taylor@bryant.io>
2024-10-11 22:46:32 -0500

    initial work

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,62 @@
-import Image from "next/image";
+import { DetailedHTMLProps, AnchorHTMLAttributes } from "react";
+
+const ExternalLink = ({
+  href,
+  children,
+  ...rest
+}: DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>) => (
+  <a className="text-[#0000EE]" href={href} {...rest}>
+    {children}
+  </a>
+);
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <p className="mb-6">
+        Hi, I&apos;m Taylor, a software engineer based in Memphis, TN. I lead
+        the web team at{" "}
+        <ExternalLink href="https://www.treatmyocd.com/">NOCD</ExternalLink>.
+      </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <p className="mb-6">
+        Some fun facts about me: I used to run an independent record label
+        called{" "}
+        <ExternalLink href="https://www.discogs.com/label/390082-Carucage-Records">
+          Carucage
+        </ExternalLink>
+        . I used to play guitar in{" "}
+        <ExternalLink href="https://tesstn.bandcamp.com/">Tess</ExternalLink>,{" "}
+        <ExternalLink href="https://neevtn.bandcamp.com/">Neev</ExternalLink>,{" "}
+        and{" "}
+        <ExternalLink href="https://siddharthatn.bandcamp.com/">
+          Siddhartha
+        </ExternalLink>
+        . I used to play drums in a couple of bands:{" "}
+        <ExternalLink href="https://closetome.bandcamp.com/">
+          Close to Me
+        </ExternalLink>{" "}
+        and{" "}
+        <ExternalLink href="https://stepsistertn.bandcamp.com/">
+          Stepsister
+        </ExternalLink>
+        .
+      </p>
+
+      <p>
+        You can also find me on{" "}
+        <ExternalLink href="https://github.com/taylorbryant">
+          GitHub
+        </ExternalLink>
+        , <ExternalLink href="https://x.com/tayl_rbryant">X</ExternalLink>, and{" "}
+        <ExternalLink href="https://linkedin.com/in/taylorjamesbryant">
+          LinkedIn
+        </ExternalLink>
+        .
+      </p>
+    </>
   );
 }
