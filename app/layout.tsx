87fc8e2e74278a6debdada577e508cb443871b64
Taylor Bryant <taylor@bryant.io>
2024-10-21 18:34:32 -0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import Image from "next/image";
 import "./globals.css";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Taylor Bryant",
