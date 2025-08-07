6b01da5fcdbe5996080e0f394817745de0eb169c
Taylor Bryant <taylor@bryant.io>
2025-08-06 20:59:26 -0500

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
     <>
       <h1 className="text-xl mb-4 font-semibold">Hi, I&apos;m Taylor.</h1>
       <p className="mb-4 text-pretty">
-        I'm a software engineer based in Memphis, TN. I lead the web team at{" "}
-        <ExternalLink href="https://www.treatmyocd.com/">NOCD</ExternalLink>,
+        I&apos;m a software engineer based in Memphis, TN. I lead the web team
+        at <ExternalLink href="https://www.treatmyocd.com/">NOCD</ExternalLink>,
         where we&apos;re helping people with OCD by raising awareness and
         providing effective treatment.
       </p>
